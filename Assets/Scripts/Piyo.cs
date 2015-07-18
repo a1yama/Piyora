27df98bf5d012b3871d7a72240b4778ee63a1f51
a1yama <a1yama1123@gmail.com>
2015-07-18 12:26:35 +0900

    update piyo ball

---
+++ Assets/Scripts/Piyo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piyo : MonoBehaviour {
+
+    public GameObject piyoPrefab;
+    public Sprite[] piyoSprites;
+    private float spd = 2;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(showPiyo(100));
+	}
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+
+            Vector3 aTapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D aCollider2d = Physics2D.OverlapPoint(aTapPoint);
+
+            if (aCollider2d) {
+                GameObject obj = aCollider2d.transform.gameObject;
+                Destroy(obj);
+            }
+        }
+    }
+    IEnumerator showPiyo(int count) {
+        for (int i = 0; i < count; i++) {
+            float dir = Random.Range(0, 359);
+            Vector2 pos = new Vector2(0, 0);
+            GameObject piyo = Instantiate(piyoPrefab, pos, Quaternion.AngleAxis(0, Vector3.forward)) as GameObject;
+            int spriteId = Random.Range(1, 5);
+            piyo.name = "Piyo" + spriteId;
+            SpriteRenderer spriteObj = piyo.GetComponent<SpriteRenderer>();
+            spriteObj.sprite = piyoSprites[spriteId];
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/piyoMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class piyoMove : Token {
+
+	void Start () {
+        float dir = Random.Range(0, 359);
+        float spd = 5;
+        SetVelocity(dir, spd);
+	}
+	
+}
